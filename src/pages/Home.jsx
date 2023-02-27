9704e2f89068dafbbccd183cd71c86d5622c0e1e
stephane1934 <stephaneher@hotmail.fr>
2023-02-27 18:47:49 +0100

    fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import SmoothieCard from "../components/SmoothieCard";
 import supabase from "../config/supabaseClient";
 
 export default function Home() {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    async function fetchSmoothies() {
+      const { data, error } = await supabase.from("smoothies").select();
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    }
+    fetchSmoothies();
+  }, []);
 
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
