a6a181667138b82d14208988f057984043c2dfd1
stephane1934 <stephaneher@hotmail.fr>
2023-02-28 07:10:05 +0100

    fetching single record

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+
 export default function Update() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    async function fetchSmoothie() {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+        console.log(error);
+      }
+
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+      }
+    }
+    fetchSmoothie();
+  }, [id, navigate]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Smoothie Recipe</button>
+      </form>
     </div>
   );
 }
